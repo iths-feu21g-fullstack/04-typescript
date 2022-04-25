3391444a8447fd1e52fa02cfd9064d3ddf63298a
David Andersson <lejonmanen@gmail.com>
2022-04-25 10:02:16 +0200

    TypeScript project (with error)

---
+++ script.ts
@@ -0,0 +1,7 @@
+
+console.log('Hello world from a TypeScript file')
+
+let username: string
+username = true  // detta är inte okej, och TypeScript hittar felet åt oss
+
+console.log(`Welcome ${username}!`)
